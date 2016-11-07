9115d4a568aa6445291a0aba3bcec5d067125d3d
jeremiahwitt <jeremiah.witt@mail.mcgill.ca>
2016-11-07 15:26:23 -0500

    Added decl for copy cons and = overload

--- Characters/Jeremiah-s-Character/MergedManager/Belt.h
+++ Characters/Jeremiah-s-Character/MergedManager/Belt.h
@@ -1,10 +1,10 @@
 /**
- * @brief Class for belts.
- * @author Philip Brink
- * @version 0.0.1
- * @date 2016-10-20
- */
- 
+* @brief Class for belts.
+* @author Philip Brink
+* @version 0.0.1
+* @date 2016-10-20
+*/
+
 // Include guard
 #ifndef Belt_H
 #define Belt_H
@@ -13,21 +13,23 @@
 #include <string>
 #include "Item.h"
 
-class Belt: public Item {
-	protected:
-		// Belt Attributes
-		int defense;
-		DECLARE_SERIAL(Belt);
-		// Constructors
-	public:
-		Belt();
-		Belt(string name, int weight, int value, string image, 
-				array<int, 9> enchantmentValues, int defense);
-		int getDefense();
-		void incrementDefense();
-		void decrementDefense();
-		std::string toString();
-		virtual void Serialize(CArchive &ar);
+class Belt : public Item {
+protected:
+	// Belt Attributes
+	int defense;
+	DECLARE_SERIAL(Belt);
+	// Constructors
+public:
+	Belt();
+	Belt(Belt* otherBelt);
+	Belt(string name, int weight, int value, string image,
+		array<int, 9> enchantmentValues, int defense);
+	int getDefense();
+	void incrementDefense();
+	void decrementDefense();
+	std::string toString();
+	virtual void Serialize(CArchive &ar);
+	Belt* operator =(const Belt *otherBelt);
 };
 
 #endif // Include guard
