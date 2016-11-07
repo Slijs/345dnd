d3daae790983e4829c85a8744f3213925341bd8b
jeremiahwitt <jeremiah.witt@mail.mcgill.ca>
2016-11-07 15:12:25 -0500

    Added copy const and assignment operator overload

--- Characters/Jeremiah-s-Character/MergedManager/Armor.h
+++ Characters/Jeremiah-s-Character/MergedManager/Armor.h
@@ -1,9 +1,9 @@
 /**
- * @brief Class for all armor.
- * @author Philip Brink
- * @version 0.0.1
- * @date 2016-10-20
- */
+* @brief Class for all armor.
+* @author Philip Brink
+* @version 0.0.1
+* @date 2016-10-20
+*/
 
 // Include guard
 #ifndef Armor_H
@@ -14,21 +14,23 @@
 #include <afx.h>
 #include "Item.h"
 
-class Armor: public Item {
-	protected:
-		// Armor Attributes
-		int defense;
-		DECLARE_SERIAL(Armor);
-		// Constructors
-	public:
-		Armor();
-		Armor(string name, int weight, int value, string image, 
-				array<int, 9> enchantmentValues, int defense);
-		int getDefense();
-		void incrementDefense();
-		void decrementDefense();
-		std::string toString();
-		virtual void Serialize(CArchive &ar);
+class Armor : public Item {
+protected:
+	// Armor Attributes
+	int defense;
+	DECLARE_SERIAL(Armor);
+	// Constructors
+public:
+	Armor();
+	Armor(string name, int weight, int value, string image,
+		array<int, 9> enchantmentValues, int defense);
+	int getDefense();
+	void incrementDefense();
+	void decrementDefense();
+	std::string toString();
+	virtual void Serialize(CArchive &ar);
+	Armor& operator= (const Armor* otherArmor); // Operator overload
+	Armor(Armor* otherArmor); // Copy constructor
 };
 
 #endif // Include guard
