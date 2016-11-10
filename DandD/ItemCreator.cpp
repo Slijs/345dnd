7b39cf27fba58725904bf1e74f2bc272ab7b7a9a
Philip Brink <pbrink@gmail.com>
2016-11-09 19:37:56 -0500

    cleaned up after mess

--- DandD/ItemCreator.cpp
+++ DandD/ItemCreator.cpp
@@ -406,37 +406,31 @@ void ItemCreator::createItems()
 
 				// serialize 
 				CFile fileS;
-				if (!fileS.Open(_T("serializedItems.dat"), CFile::modeWrite))
-				{
-					std::cout << "Unable to open output file" << std::endl;
-					break;
 
-					if (FILE *file = fopen("serializedItems.dat", "r")) {
-						fclose(file);
-						if (!fileS.Open(_T("serializedItems.dat"), CFile::modeWrite))
-						{
-							std::cout << "Unable to open output file" << std::endl;
-							break;
-						}
-						cout << "Saving items...\n";
-						CArchive arStore(&fileS, CArchive::store);
-						userContainer->Serialize(arStore);
-						arStore.Close();
-						fileS.Close();
+				if (FILE *file = fopen("serializedItems.dat", "r")) {
+					fclose(file);
+					if (!fileS.Open(_T("serializedItems.dat"), CFile::modeWrite))
+					{
+						std::cout << "Unable to open output file" << std::endl;
+						break;
 					}
-					else {
-						if (!fileS.Open(_T("serializedItems.dat"), CFile::modeWrite | CFile::modeCreate))
-						{
-							std::cout << "Unable to open output file" << std::endl;
-							break;
-						}
-						cout << "Saving items...\n";
-						CArchive arStore(&fileS, CArchive::store);
-						userContainer->Serialize(arStore);
-						arStore.Close();
-						fileS.Close();
+					cout << "Saving items...\n";
+					CArchive arStore(&fileS, CArchive::store);
+					userContainer->Serialize(arStore);
+					arStore.Close();
+					fileS.Close();
+				}
+				else {
+					if (!fileS.Open(_T("serializedItems.dat"), CFile::modeWrite | CFile::modeCreate))
+					{
+						std::cout << "Unable to open output file" << std::endl;
+						break;
 					}
-
+					cout << "Saving items...\n";
+					CArchive arStore(&fileS, CArchive::store);
+					userContainer->Serialize(arStore);
+					arStore.Close();
+					fileS.Close();
 				}
 			} 
 			else if (number == 10) {
